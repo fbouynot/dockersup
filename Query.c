2795bdf181939e4249cbaf4f237de6e09b924178
agadagluglu <alex.punch@netcourrier.com>
2016-07-21 20:51:30 +0200

    Fixed server_ip pointer not properly set.

--- Query.c
+++ Query.c
@@ -10,17 +10,18 @@
 
 void Query()
 {
-    char* server_ip;
-    FILE * fp;
+    char* server_ip = malloc(sizeof(char) * 1024);
+FILE * fp;
     fp = fopen("ip.conf", "r");
     if (fp == NULL) {
         printf ("Echec d'ouverture du fichier, erreur numéro %d\n", errno);
         exit(-1);
     }
     // On lit le ficher de configuration qui contient l'IP du serveur cible
-    // "%29[^\n]" signifie que fscnaf lira tous les carracteres de fp sauf '\n'
-    // et s'arretera s'il le rencontre
-    fscanf(fp, "%29[^\n]", server_ip);
+    // "%15[^\n]" signifie que fscnaf lira tous les carracteres de fp sauf '\n'
+    // et s'arretera s'il le rencontre ou apres les 15er carracteres.
+    fscanf(fp, "%15[^\n]", server_ip);
     printf(Client(server_ip));
     fclose(fp);
+    free(server_ip);
 }
