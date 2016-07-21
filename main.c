5231740c11433db58fcc72b7588591b2e2a9a75b
felix <felixbouynot@gmail.com>
2016-07-21 21:19:23 +0200

    Cleanup des fichiers pour travailler en collaboration

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
-#include "ClientTCP.h"
 #include "ServerTCP.h"
-#include "Getinfo.h"
-#include "Felix.h"
-#include "Alex.h"
+#include "Menu.h"
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    Felix();
-    Alex();
+    pid_t pid;
+    pid = fork();
+    if (pid == -1)
+    {
+        printf("Le fork a échoué, le client et le serveur ne peuvent pas s'exécuter en même temps.\n");
+        exit(-1);
+    }
+    else if (pid == 0)
+    {
+        Server();
+    }
+    else
+    {
+        sleep(1);
+        Menu();
+    }
 
     return 0;
 }
