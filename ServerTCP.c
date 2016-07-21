ff9333fc708f23b205a1d5921b123460954b2bec
felix <felixbouynot@gmail.com>
2016-07-21 16:22:27 +0200

    include

--- ServerTCP.c
+++ ServerTCP.c
@@ -7,22 +7,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <signal.h>
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/time.h>
 #include <stdlib.h>
-#include <memory.h>
-#include <ifaddrs.h>
-#include <net/if.h>
-#include <stdarg.h>
-#include <string.h>
 
 int Server()
 {
