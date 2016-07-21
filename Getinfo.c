99d21e05953c196e4fb52ce318a2ab79f84be485
felix <felixbouynot@gmail.com>
2016-07-21 21:34:00 +0200

    Commentaires

--- Getinfo.c
+++ Getinfo.c
@@ -10,6 +10,8 @@
 char* GetInfo() {
     char buff[BUFSIZ];
     char* msg_popen = malloc(sizeof(char) * 1024);
+    /* On utilise la fonction système "docker ps -a" pour récupérer les informations voulues.
+     * popen nous permet de récupérer le retour de la fonction dans une variable et non dans stdout */
     FILE *fp = popen("docker ps -a", "r");
     while (fgets(buff, BUFSIZ, fp) != NULL) {
         strcat(msg_popen, buff);
